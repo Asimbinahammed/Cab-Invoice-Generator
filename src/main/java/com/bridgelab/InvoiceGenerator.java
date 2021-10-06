b392cd36968855fb34155d783298c535c59bbb55
asimbinahammed <asim242370@gmail.com>
2021-10-06 10:19:26 +0530

    Setting cab invoice generator

---
+++ src/main/java/com/bridgelab/InvoiceGenerator.java
@@ -0,0 +1,65 @@
+/**
+ * Purpose : To simulate Cab Service
+ * @author ASIM AHAMMED
+ */
+
+package com.bridgelab;
+
+public class InvoiceGenerator {
+    // Constants
+    private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_COST_PER_KILOMETER = 10.0;
+    private static final double MINIMUM_FARE = 5.0;
+    private final RideRepository rideRepository;
+
+    public InvoiceGenerator() {
+        this.rideRepository = new RideRepository();
+    }
+
+    /**
+     * Purpose : To calculate the minimum fare of a journey
+     *
+     * @param distance : takes distance as a first parameter
+     * @param time     : takes time as a second parameter
+     * @return the total fare or minimum fare as per expectations
+     */
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        return Math.max(totalFare, MINIMUM_FARE);
+    }
+
+    /**
+     * Purpose : To calculate total fare for multiple rides
+     *
+     * @param rides : takes multiple rides as first parameter
+     * @return aggregate total fare for all rides.
+     */
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride : rides) {
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        }
+        return totalFare;
+    }
+
+    /**
+     * Purpose : To calculate total fare for multiple rides
+     * @param rides : takes multiple rides as first parameter
+     * @return the total fare and the length of rides
+     */
+    public InvoiceSummary calculateFareSummary(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride : rides) {
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        }
+        return new InvoiceSummary(rides.length, totalFare);
+    }
+
+    public InvoiceSummary getInvoiceSummary(String userId) {
+        return this.calculateFareSummary(rideRepository.getRides(userId));
+    }
+
+    public void addRides(String userId, Ride[] rides) {
+        rideRepository.addRides(userId, rides);
+    }
+}
---
+++ src/test/java/com/bridgelab/InvoiceServiceTest.java
@@ -0,0 +1,61 @@
+package com.bridgelab;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class InvoiceServiceTest {
+    public static InvoiceGenerator invoiceGenerator = null;
+
+    @BeforeAll
+    static void beforeAll() {
+        invoiceGenerator = new InvoiceGenerator();
+    }
+
+    @Test
+    void givenDistanceAndTimeShouldReturnTheTotalFare() {
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(25, fare, 0.0);
+    }
+
+    @Test
+    void givenLessDistanceAndTimeShouldReturnTheMinimumFare() {
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(5, fare, 0.0);
+    }
+
+    @Test
+    void givenMultipleRidesShouldReturnTotalFare() {
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assertions.assertEquals(30, fare, 0.0);
+    }
+
+    @Test
+    void givenMultipleRidesShouldReturnInvoiceSummary() {
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        InvoiceSummary invoiceSummary = invoiceGenerator.calculateFareSummary(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+        Assertions.assertEquals(expectedInvoiceSummary, invoiceSummary);
+    }
+
+    @Test
+    void givenUserIDAndRideListShouldReturnInvoiceSummary() {
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        String userId = "CRN 1001";
+        invoiceGenerator.addRides(userId, rides);
+        InvoiceSummary invoiceSummary = invoiceGenerator.getInvoiceSummary(userId);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+        Assertions.assertEquals(expectedInvoiceSummary, invoiceSummary);
+    }
+}
